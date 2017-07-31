539efb7716443da820e85dbdae002253da0ac9c7
Raymund Micoh Alvarez <alvarezmicoh@gmail.com>
2017-07-31 23:53:39 +0800

    CoreNLP

--- src/Driver.java
+++ src/Driver.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 
 import org.apache.log4j.BasicConfigurator;
 
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import gate.Annotation;
 import gate.AnnotationSet;
 import gate.Corpus;
@@ -20,7 +21,7 @@ import gate.Utils;
 public class Driver {
 
 
-		public static void main(String[] args) throws IOException, GateException {
+		public static void main(String[] args) throws IOException, GateException, ClassNotFoundException {
 			// TODO Auto-generated method stub
 		
 			
@@ -30,18 +31,18 @@ public class Driver {
 			write.pars(read.getStdControl(), read.getNumber(),read.getControl(),read.getAsset(),read.getOrg(),read.getVulnerable(),
 					read.getSecurity(),read.getSeverity(),read.getThreat(),read.getThreatOrigin(),read.getThreatSource());
 			
-			BasicConfigurator.configure();
-		        Gate.init();
-
-		        LanguageAnalyser controller = (LanguageAnalyser) PersistenceManager
-		                .loadObjectFromFile(new File(new File(Gate.getPluginsHome(),
-		                        ANNIEConstants.PLUGIN_DIR), ANNIEConstants.DEFAULT_FILE));
-		     
-		        Corpus corpus = Factory.newCorpus("corpus");
-		        Document document = Factory.newDocument(
-		                 read.getSentence().get(1));
-		        corpus.add(document); controller.setCorpus(corpus); 
-		        controller.execute();
+//			BasicConfigurator.configure();
+//		        Gate.init();
+//
+//		        LanguageAnalyser controller = (LanguageAnalyser) PersistenceManager
+//		                .loadObjectFromFile(new File(new File(Gate.getPluginsHome(),
+//		                        ANNIEConstants.PLUGIN_DIR), ANNIEConstants.DEFAULT_FILE));
+//		     
+//		        Corpus corpus = Factory.newCorpus("corpus");
+//		        Document document = Factory.newDocument(
+//		                 read.getSentence().get(1));
+//		        corpus.add(document); controller.setCorpus(corpus); 
+//		        controller.execute();
 
 //		        document.getAnnotations().get(new HashSet<>(Arrays.asList("Token", "Person", "Location")))
 //		            .forEach(a -> System.err.format("%s - \"%s\" [%d to %d]\n", 
@@ -52,23 +53,33 @@ public class Driver {
 //		        Factory.deleteResource(document); Factory.deleteResource(corpus); Factory.deleteResource(controller);
 		
 		        // POS TAGGER
-			AnnotationSet tokens = document.getAnnotations();
-			
-			System.out.println(read.getSentence().get(0));
-			for(Annotation t : tokens)
-			  {
-				
-				if(t.getFeatures().get("kind") != null &&
-						t.getFeatures().get("kind").toString().equalsIgnoreCase("word"))
-					   System.out.println("POS TAG:" + t.getFeatures().get("category").toString());
+//			AnnotationSet tokens = document.getAnnotations();
+//			
+//			System.out.println(read.getSentence().get(0));
+//			for(Annotation t : tokens)
+//			  {
+//				
+//				if(t.getFeatures().get("kind") != null &&
+//						t.getFeatures().get("kind").toString().equalsIgnoreCase("word"))
+//					   System.out.println("POS TAG:" + t.getFeatures().get("category").toString());
 			   
 			 /* 
 			  If looking for specific features go for
 			  System.out.println( t.getFeatures().get("FeatureName").toString() );
 			 */
 
-			
-			  }
+			// CORE NLP Pos Tagger
+				MaxentTagger tagger = new MaxentTagger("C:\\Users\\Micoh F Alvarez\\Documents\\IESystem\\tagger\\left3words-wsj-0-18.tagger");
+				String sample = "This is a sample text";
+				 
+				// The tagged string
+				 
+				String tagged = tagger.tagString(read.getSentence().get(0));
+				 
+				// Output the result
+				 
+				System.out.println(tagged);
+			  
 			
 			
 		}
